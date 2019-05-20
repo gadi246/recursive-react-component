8344ecdeb64558f9bea2cceaa780444ee5e7a264
Gadi Tzkhori <gadi@infibond.com>
2019-05-20 10:15:05 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const Cube = props => <div {...props} style={{width:20, height:20, background: 'red'}}/>;
+class List extends React.Component{
+  state = {
+    cubes: this.props.cubes && [{children: false}]
+  };
+  handleClick = index => {
+    const clickedCubeHasChildren = this.state.cubes[index].children;
+    if(clickedCubeHasChildren){
+     return this.setState({cubes: [...this.state.cubes, {children: false}]});
+    }
+    this.setState(({cubes}) => ({cubes: cubes.map((cube, i) => i === index ? {...cube, children: true} : cube )}))
+  };
+    render(){
+      return(
+          <ul>
+              {this.state.cubes.map(({children}, i) => {
+                return <li><Cube onClick={() => this.handleClick(i)}/>{ children && <List cubes={children}/>}</li>;
+              })}
+          </ul>
+      )
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <List cubes={true}/>
     </div>
   );
 }
